763469deaccc8c9e18ae15644c0b1177cb8012b2
yzhang111 <96157421+yzhang111@users.noreply.github.com>
2026-07-22 18:41:43 +0900

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        max_profit = 0
+        for price in prices:
+            if price - min_price > max_profit:
+                max_profit = price - min_price
+            if min_price > price:
+                min_price = price
+
+        return max_profit
